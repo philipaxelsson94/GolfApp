5729a80418af8be30ae54fdb0620e9ffe905154d
Philip <philipax@kth.se>
2022-07-07 10:29:44 +0200

    Add files

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
+
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
+
 const SignIn = () => {
 
 return(
